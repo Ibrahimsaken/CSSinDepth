557a0c94df5d958aadeff97126e98b18287778f8
ibrahimsaken <ibrahimsaken00@gmail.com>
2020-08-19 18:32:02 +0600

    master

---
+++ 9. Модульный CSS/js/script.js
@@ -0,0 +1,8 @@
+(function () {
+   var toggle = document.querySelector('.dropdown__toggle');
+   toggle.addEventListener('click', function (event) {
+      event.preventDefault();
+      var dropdown = event.target.parentNode;
+      dropdown.classList.toggle('is-open');
+   });
+}());
