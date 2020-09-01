db8655537e7d1c522fad64d507090ad50a496f5c
Ibrahimsaken <ibrahimsaken00@gmail.com>
2020-09-01 22:07:35 +0600

    animation++

---
+++ 16. Анимации/16.4 Анимация формы отправки/js/script.js
@@ -0,0 +1,23 @@
+var input = document.getElementById('trip');
+var button = document.getElementById('submit-button');
+
+var timeout = null;
+
+button.addEventListener('click', function (event) {
+   event.preventDefault();
+   clearTimeout(timeout);
+   button.classList.add('is-loading');
+   button.disabled = true;
+   input.disabled = true;
+});
+
+input.addEventListener('keyup', function () {
+   clearTimeout(timeout);
+   timeout = setTimeout(function () {
+      button.classList.add('shake');
+   }, 1000);
+});
+
+button.addEventListener('animationed', function () {
+   button.classList.remove('shake');
+});
